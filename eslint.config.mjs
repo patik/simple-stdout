f054d94e385713a6cb2f274572710d09d6fc1897
Craig Patik <craig@patik.com>
2025-12-26 18:29:54 +0100

    fix eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -17,7 +17,7 @@ export default tseslint.config(
         languageOptions: {
             parserOptions: {
                 project: ['./tsconfig.json'],
-                tsconfigRootDir: '.',
+                tsconfigRootDir: import.meta.dirname,
             },
         },
         rules: {

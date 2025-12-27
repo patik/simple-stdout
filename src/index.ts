9524e5aa601fd5ecbdde08be40633b5377168459
Craig Patik <craig@patik.com>
2025-12-27 13:08:26 +0100

    Add support for execFile (#97)

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,40 @@
-import { exec } from 'node:child_process'
+#!/usr/bin/env node
+
+import type { ExecException, ExecFileException } from 'node:child_process'
+import { exec, execFile } from 'node:child_process'
+
+const callback =
+    (resolve: (value: string | PromiseLike<string>) => void, reject: (reason?: unknown) => void) =>
+    (err: ExecException | ExecFileException | null, stdout: string, stderr: string) => {
+        if (err || stderr) {
+            reject(err || new Error(stderr))
+        } else {
+            resolve(
+                stdout
+                    // Remove trailing newline
+                    .replace(/\n$/, ''),
+            )
+        }
+    }
 
 /**
  * Returns stdout from the given shell command
+ *
+ * Calls `child_process.exec` under the hood.
  */
 export default async function stdout(command: string): Promise<string> {
     return new Promise((resolve, reject) => {
-        exec(command, (err, stdout, stderr) => {
-            if (err || stderr) {
-                reject(err || new Error(stderr))
-            } else if (typeof stdout === 'string') {
-                resolve(stdout.replace(/\n$/, '')) // Remove trailing newline
-            }
-        })
+        exec(command, callback(resolve, reject))
+    })
+}
+
+/**
+ * Returns stdout from executing the given file/executable
+ *
+ * Calls `child_process.execFile` under the hood.
+ */
+export async function stdoutFile(file: string, args: string[] = []): Promise<string> {
+    return new Promise((resolve, reject) => {
+        execFile(file, args, callback(resolve, reject))
     })
 }
